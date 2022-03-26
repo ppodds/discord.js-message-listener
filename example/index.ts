0f22239cb1325187a8945331b020475e68783c3a
ppodds <oscar20020629@gmail.com>
2022-03-27 02:35:12 +0800

    Feat: add paginator example

--- example/index.ts
+++ example/index.ts
@@ -1,12 +1,14 @@
 import {
+    ButtonInteraction,
     Client,
     Intents,
     MessageActionRow,
     MessageButton,
+    MessageEmbed,
     MessageSelectMenu,
 } from "discord.js";
 import { MessageButtonStyles } from "discord.js/typings/enums";
-import { ActionRowMessageListener } from "../lib/index";
+import { ActionRowMessageListener, Paginator } from "../lib/index";
 import { token } from "./env";
 
 const client = new Client({
@@ -38,29 +40,62 @@ client.on("ready", () => {
 });
 
 client.on("messageCreate", async (message) => {
-    if (message.content !== "!test") return;
-    const msg = await message.channel.send("test");
-    const messageActionRow1 = new MessageActionRow();
-    const btn1 = new MessageButton();
-    const btn2 = new MessageButton();
-    const btn3 = new MessageButton();
-    btn1.setLabel("1");
-    btn2.setLabel("2");
-    btn2.setURL("https://www.google.com");
-    btn3.setLabel("3");
-    btn3.setStyle(MessageButtonStyles.SUCCESS);
-    const messageActionRow2 = new MessageActionRow();
-    const selectMenu = new MessageSelectMenu();
-    selectMenu.addOptions({ label: "a", value: "a" });
-    selectMenu.addOptions({ label: "b", value: "b" });
-    messageActionRow1.addComponents([btn1, btn2, btn3]);
-    messageActionRow2.addComponents([selectMenu]);
-    const listener = new ActionRowMessageListener(msg, {
-        messageActionRows: [messageActionRow1, messageActionRow2],
-    });
-    listener.on("end", () => console.log("listenerEnd"));
-    listener.on("collect", (arg) => console.log("collect", arg));
-    listener.on("collectError", (error) => console.log(error));
+    if (message.content === "!test") {
+        const msg = await message.channel.send("test");
+        const messageActionRow1 = new MessageActionRow();
+        const btn1 = new MessageButton();
+        const btn2 = new MessageButton();
+        const btn3 = new MessageButton();
+        btn1.setLabel("1");
+        btn2.setLabel("2");
+        btn2.setURL("https://www.google.com");
+        btn3.setLabel("3");
+        btn3.setStyle(MessageButtonStyles.SUCCESS);
+        const messageActionRow2 = new MessageActionRow();
+        const selectMenu = new MessageSelectMenu();
+        selectMenu.addOptions({ label: "a", value: "a" });
+        selectMenu.addOptions({ label: "b", value: "b" });
+        messageActionRow1.addComponents([btn1, btn2, btn3]);
+        messageActionRow2.addComponents([selectMenu]);
+        const listener = new ActionRowMessageListener(msg, {
+            messageActionRows: [messageActionRow1, messageActionRow2],
+            collectorOptions: {
+                time: 10000,
+            },
+        });
+        listener.on("end", () => console.log("listenerEnd"));
+        listener.on("collect", (arg) => console.log("collect", arg));
+        listener.on("collectError", (error) => console.log(error));
+    } else if (message.content === "!paginator") {
+        const msg = await message.channel.send("paginator");
+        const messageActionRow = new MessageActionRow();
+        const prevPage = new MessageButton();
+        const nextPage = new MessageButton();
+        prevPage.setLabel("Prev");
+        prevPage.setCustomId("prev-btn");
+        nextPage.setLabel("Next");
+        nextPage.setCustomId("next-btn");
+        messageActionRow.addComponents([prevPage, nextPage]);
+        const listener = new ActionRowMessageListener(msg, {
+            messageActionRows: [messageActionRow],
+            collectorOptions: {
+                time: 60000,
+            },
+        });
+        const page1 = new MessageEmbed();
+        const page2 = new MessageEmbed();
+        page1.setTitle("Page 1");
+        page2.setTitle("Page 2");
+        const pages = [page1, page2];
+        const paginator = new Paginator(listener, {
+            pages,
+            nextPageFilter: (arg) =>
+                (arg as ButtonInteraction).customId === "next-btn",
+            previousPageFilter: (arg) =>
+                (arg as ButtonInteraction).customId === "prev-btn",
+        });
+        await paginator.start();
+    }
 });
 
 client.login(token);
